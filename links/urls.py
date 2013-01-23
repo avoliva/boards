9918d947f7a478c473fbbf64adbc9e05815ac962
avoliva <adamvoliva@gmail.com>
2013-01-23 04:40:17 -0800

    Fixed the links. South still broken.

--- links/urls.py
+++ links/urls.py
@@ -10,13 +10,13 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = patterns('',
 
 	# New links, created in the last 28 days.
-	url(r'^$', login_required(LinksListView.as_view(
-		queryset=Links.objects.filter(
+	url(r'^$', login_required(LinkListView.as_view(
+		queryset=Link.objects.filter(
 			pub_date__gte=datetime.now()-timedelta(days=28)))), 
 		name='links'),
 
 	# Specific link
-	url(r'^(?P<pk>\d+)[/]$', login_required(LinksDetailView.as_view()), 
+	url(r'^(?P<pk>\d+)[/]$', login_required(LinkDetailView.as_view()), 
 		name='link'),
 
 	# Post a message to a link
@@ -24,21 +24,21 @@ urlpatterns = patterns('',
 		name='create_message'),
 
 	# Link Creation
-	url(r'^create[/]$', login_required(LinksCreateView.as_view()), name='link_create'),
+	url(r'^create[/]$', login_required(LinkCreateView.as_view()), name='link_create'),
 
 	# Top rated links of all time
-	url(r'^top[/]$', login_required(LinksListView.as_view(
-		queryset=Links.objects.order_by("-rank"))),
+	url(r'^top[/]$', login_required(LinkListView.as_view(
+		queryset=Link.objects.order_by("-rank"))),
 		name='top_rank'),
 
 	# Top rated links of the week
-	url(r'^top_week[/]$', login_required(LinksListView.as_view(
-		queryset=Links.objects.filter(
+	url(r'^top_week[/]$', login_required(LinkListView.as_view(
+		queryset=Link.objects.filter(
 			pub_date__gte=datetime.now()-timedelta(days=7)).order_by('-rank'))),
 		name='top_week'),
 
 	# All links
-	url(r'^all[/]$', login_required(LinksListView.as_view()), name='all_links'),
+	url(r'^all[/]$', login_required(LinkListView.as_view()), name='all_links'),
 
 	# User list
 	url(r'^user_list[/]$', login_required(UserListView.as_view(
@@ -50,10 +50,10 @@ urlpatterns = patterns('',
 
 	# Login page
 	url(r'^login[/]$', 'django.contrib.auth.views.login', 
-		{'template_name': 'boards/login.html'}),
+		{'template_name': 'links/login.html'}),
 
 	# Logout page
 	url(r'^logout[/]$', 'django.contrib.auth.views.logout', 
-		{'template_name': 'boards/logout.html',
-		 'next_page' : '/boards/login'}),
+		{'template_name': 'links/logout.html',
+		 'next_page' : '/links/login'}),
 )
